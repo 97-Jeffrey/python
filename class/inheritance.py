aec9bc34f17173870588f42de1821bc24d3332bb
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-01-14 14:30:04 -0800

    few more classes are created

--- class/inheritance.py
+++ class/inheritance.py
@@ -23,3 +23,18 @@ class Fish(Animal):
 
 m = Mammal()
 print(m.age)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+p1 = Point(1, 2)
+p2 = Point(1, 2)
+
+print(p1 == p2)
